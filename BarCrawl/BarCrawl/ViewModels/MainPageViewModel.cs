2c34ccbc40c2493afe69dd62241d8ab8e0a3f52f
Joel Pickin <joelpickin@outlook.com>
2020-12-21 11:12:58 +0000

    Add project files.

---
+++ BarCrawl/BarCrawl/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BarCrawl.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+    }
+}
---
+++ BarCrawl/BarCrawl/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BarCrawl.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
