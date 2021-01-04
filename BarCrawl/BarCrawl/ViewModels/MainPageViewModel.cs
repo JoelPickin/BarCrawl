7977e764b5deb25d93c3f34050f62e2424f6a70a
Joel Pickin <joelpickin@outlook.com>
2021-01-04 15:37:43 +0000

    Initial Commit

--- BarCrawl/BarCrawl/ViewModels/MainPageViewModel.cs
+++ BarCrawl/BarCrawl/ViewModels/MainPageViewModel.cs
@@ -12,13 +12,27 @@ namespace BarCrawl.ViewModels
     {
         public string Location { get; set; }
         public DateTime Date { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string TestLabel { get; set; }
 
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            InitialSetup();
+        }
+
+        private void InitialSetup()
+        {
+            Location = "Newcastle";
+            Date = DateTime.Now;
+            StartTime = TimeSpan.FromHours(12);
+            EndTime = TimeSpan.FromHours(16);
         }
     }
 }
